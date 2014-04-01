c4f051307ef2d125b63a0e4316e9c2a3f30ad482
Gregory Mitrano <blairmlh@gmail.com>
2014-04-01 15:34:04 -0400

    Added initial functionality to zzz for calculating what time to wake up

--- zzz.py
+++ zzz.py
@@ -1,5 +1,20 @@
 #!/bin/python
 # zzz
 
+import argparse
+import datetime
+
 if __name__ == '__main__':
-    print('zzz')
+    parser = argparse.ArgumentParser(description='Calculates wake up times based on sleep cycles')
+    parser.add_argument('--cycles', default=6, type=int, help='number of sleep cycles to calculate')
+    parser.add_argument('--sleepDelay', default=15, type=int, help='offset in minutes used to compensate for the time taken to fall asleep')
+    parser.add_argument('--cycleLength', default=90, type=int, help='length in minutes of a complete sleep cycle')
+    args = parser.parse_args()
+
+    sleepDelay = datetime.timedelta(minutes=args.sleepDelay)
+    cycleDelta = datetime.timedelta(minutes=args.cycleLength)
+    currentTime = datetime.datetime.now()
+
+    for cycle in range(1, args.cycles + 1):
+        cycleEndTime = currentTime + (cycleDelta * cycle)
+        print("%d Cycle(s): Wake up at %s" % (cycle, cycleEndTime.strftime("%I:%H%p")))
